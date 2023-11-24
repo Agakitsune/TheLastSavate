e577c6017a9227033287e5b76174899a87c24839
Turpin Raphaël <raphael.turpin@epitech.eu>
2023-11-24 21:02:58 +0400

    Some experience

--- Camera2D.gd
+++ Camera2D.gd
@@ -8,5 +8,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_released("zoom_in") and zoom.x < 4:
+		zoom = get("zoom") + Vector2(0.5, 0.5)
+	if Input.is_action_just_released("zoom_out") and zoom.x > 2:
+		zoom = get("zoom") - Vector2(0.5, 0.5)
 	position = follow.position
 	pass
