85401a94bd8b48add08f1e75fe4b10f93e56e232
feak marcolo <said.yassin@epitech.eu>
2023-11-25 17:50:34 +0400

    deseperatly trying to do a f***** movement

---
+++ scenes/TileMap.gd
@@ -0,0 +1,12 @@
+extends TileMap
+
+var selectedTile
+var baseTile
+
+func _ready():
+	baseTile = map_to_local(Vector2(15,15))
+	
+func _process(_delta):
+	var tile = local_to_map(get_global_mouse_position())
+	selectedTile = map_to_local(tile)
+	set_cell(0, Vector2(15,15), 0, Vector2(7,3), 0)
