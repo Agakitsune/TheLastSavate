1355cb21ecc40d550798435857e6b0eb352a8543
feak marcolo <said.yassin@epitech.eu>
2023-11-26 12:48:26 +0400

    finished

--- splash.gd
+++ splash.gd
@@ -7,14 +7,7 @@ const SOUND = preload("res://Quaso_Audio_Cut..mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("test")
-	
 	$AnimationPlayer.play("fadeToNormal")
-
-func do_this():
-	print("yo")
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,3 +18,5 @@ func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fadeToNormal":
 		splashSound.play()
 		$AnimationPlayer.play("fadeToBlack")
+	else:
+		get_tree().change_scene_to_file("res://main_menu.tscn")
