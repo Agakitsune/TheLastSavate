c3dd8f8df51b3041c427078dc549caeb579c66e1
Turpin Raphaël <raphael.turpin@epitech.eu>
2023-11-26 10:36:13 +0400

    Ui

--- Main Menu.gd
+++ Main Menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://in_game_ui.tscn")
+	get_tree().change_scene_to_file("res://scenes/in_game_ui.tscn")
 
 
 func _on_options_button_pressed():
